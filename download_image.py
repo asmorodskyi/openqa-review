c22bf5027b0278135699dfba18374c1dfd7fa714
Anton Smorodskyi <asmorodskyi@suse.de>
2024-03-01 11:36:37 +0100

    Follow up on latest code cleanup (ca584ed4e37d2a8413d88805a92828fe75d154d8)

--- download_image.py
+++ download_image.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
 
-from bs4 import BeautifulSoup
-from myutils import openQAHelper
-from urllib.request import urlopen
 import re
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+from myutils import TaskHelper
 
 
-class ImageUploader(openQAHelper):
+class ImageUploader(TaskHelper):
 
     def __init__(self):
-        super(ImageUploader, self).__init__('ImageUploader')
+        super(ImageUploader, self).__init__("ImageUploader", False)
         self.url = "https://download.suse.de/ibs/Devel:/PubCloud:/Stable:/CrossCloud:/SLE15-SP3:/ModifiedTestImages/images/"
         self.upload_job_id_query = "select max(id) from jobs where group_id='274'  and test='publiccloud_upload_img' and arch='x86_64' and flavor='EC2';"
         self.clone_job_cmd = "/usr/share/openqa/script/clone_job.pl --skip-chained-deps --parental-inheritance --from https://openqa.suse.de --host http://autobot.qa.suse.de  {} WORKER_CLASS=qemu_x86_64 PUBLIC_CLOUD_IMAGE_LOCATION={}"
 
     def run(self):
         with urlopen(self.url) as response:
-            soup = BeautifulSoup(response.read(), 'html.parser')
-            for a in soup.findAll('a', href=True):
-                m = re.match(r"\.\/(SLES15-SP3-Lasso-BYOS.x86_64-[\d\.]+-EC2-HVM-Build[\d\.]+raw.xz)$", a['href'])
+            soup = BeautifulSoup(response.read(), "html.parser")
+            for a in soup.findAll("a", href=True):
+                m = re.match(
+                    r"\.\/(SLES15-SP3-Lasso-BYOS.x86_64-[\d\.]+-EC2-HVM-Build[\d\.]+raw.xz)$",
+                    a["href"],
+                )
                 if m:
                     full_url = "{}{}".format(self.url, m.group(1))
                     self.logger.info("Will upload {} \n".format(full_url))
                     rez = self.osd_query(self.upload_job_id_query)
-                    self.shell_exec(self.clone_job_cmd.format(rez[0][0], full_url), log=True)
+                    self.shell_exec(self.clone_job_cmd.format(rez[0][0], full_url))
                     return 0
 
 
